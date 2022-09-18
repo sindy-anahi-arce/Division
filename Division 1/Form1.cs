e815b7e2c7e81e08052de42dfe1ebe2c6245c558
sindy <sindy@SINDY_ARCE>
2022-09-17 23:34:56 -0600

    Agregar archivos de proyecto.

---
+++ Division 1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Division_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDivi_Click(object sender, EventArgs e)
+        {
+            clDivi divi = new clDivi(float.Parse(txtNum1.Text), float.Parse(txtNum2.Text));
+            textBox3.Text = divi.division().ToString();
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            txtNum1.Clear();
+            txtNum2.Clear();
+            textBox3.Clear();
+        }
+    }
+}
---
+++ Division 1/clDivi.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Division_1
+{
+    class clDivi
+    {
+        //Atributos
+        private float num1;
+        private float num2;
+
+        //Constructor(Es el que dirige)
+        public clDivi(float num1, float num2)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+        }
+        //Método
+        public float division()
+        {
+            return (num1 / num2);
+        }
+    }
+}
